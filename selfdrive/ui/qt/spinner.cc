338d67e5c404a28984546ac4e9f47a94f71045d3
egreen-park <egreen_park@naver.com>
2024-07-21 00:53:47 +0900

    AllNew K7 LDWS 24/07/20

--- selfdrive/ui/qt/spinner.cc
+++ selfdrive/ui/qt/spinner.cc
@@ -16,22 +16,23 @@
 
 TrackWidget::TrackWidget(QWidget *parent) : QWidget(parent) {
   setAttribute(Qt::WA_OpaquePaintEvent);
-  setFixedSize(spinner_size);
+  //setFixedSize(spinner_size);
+  setFixedSize(spinner_BG_size);
 
   // pre-compute all the track imgs. make this a gif instead?
-  QPixmap comma_img = loadPixmap("../assets/img_spinner_comma.png", spinner_size);
-  QPixmap track_img = loadPixmap("../assets/img_spinner_track.png", spinner_size);
+  QPixmap comma_img = loadPixmap("../assets/img_spinner_comma.png", spinner_BG_size);
+  QPixmap track_img = loadPixmap("../assets/img_spinner_track.png", spinner_BG_size);
 
   QTransform transform(1, 0, 0, 1, width() / 2, height() / 2);
-  QPixmap pm(spinner_size);
+  QPixmap pm(spinner_BG_size);
   QPainter p(&pm);
   p.setRenderHint(QPainter::SmoothPixmapTransform);
   for (int i = 0; i < track_imgs.size(); ++i) {
     p.resetTransform();
-    p.fillRect(0, 0, spinner_size.width(), spinner_size.height(), Qt::black);
+    p.fillRect(0, 0, spinner_BG_size.width(), spinner_BG_size.height(), Qt::black);
     p.drawPixmap(0, 0, comma_img);
-    p.setTransform(transform.rotate(360 / spinner_fps));
-    p.drawPixmap(-width() / 2, -height() / 2, track_img);
+    //p.setTransform(transform.rotate(360 / spinner_fps)); 제거
+    //p.drawPixmap(-width() / 2, -height() / 2, track_img); 제거
     track_imgs[i] = pm.copy();
   }
 
@@ -53,7 +54,8 @@ void TrackWidget::paintEvent(QPaintEvent *event) {
 Spinner::Spinner(QWidget *parent) : QWidget(parent) {
   QGridLayout *main_layout = new QGridLayout(this);
   main_layout->setSpacing(0);
-  main_layout->setMargin(200);
+  //main_layout->setMargin(200);
+  main_layout->setContentsMargins(0, 0, 0, 200); // 스피너 제거 배경추가 24/07/20
 
   main_layout->addWidget(new TrackWidget(this), 0, 0, Qt::AlignHCenter | Qt::AlignVCenter);
 
--- selfdrive/ui/qt/spinner.h
+++ selfdrive/ui/qt/spinner.h
@@ -8,7 +8,8 @@
 #include <QWidget>
 
 constexpr int spinner_fps = 30;
-constexpr QSize spinner_size = QSize(360, 360);
+//constexpr QSize spinner_size = QSize(360, 360); 스피너 삭제 24/07/20
+constexpr QSize spinner_BG_size = QSize(1920, 1080); // 스피너 배경이미지 추가 24/7/20
 
 class TrackWidget : public QWidget  {
   Q_OBJECT
